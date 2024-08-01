3e1f1f8b7cc87dd31daa56029c99951d068746cb
David Morales <david.morales@ppm.com.ec>
2024-07-31 18:30:57 -0600

    Create math operations

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * this method sum 2 numbers
+ * @param {number} firstNumber
+ * @param {number} secondNumber
+ * @return {number}
+ */
+const add = (firstNumber, secondNumber) =>{
+    return firstNumber + secondNumber;
+}
+
+module.exports = {
+    add
+}
