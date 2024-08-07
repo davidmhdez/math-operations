666491118cd04fb1513677cb7de4afc121465ef8
David Morales <david.morales@ppm.com.ec>
2024-08-07 13:03:48 -0600

    Add subtract operation and implements number type validation

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 /**
+ *  Validate if all params are numbers
+ *  @param {...number} numbers
+ */
+const numberValidation = (...numbers) =>{
+    if(numbers.some(number=> typeof(number) !== 'number')){
+        throw new Error(`Some numbers is not valid`);
+    }
+}
+
+/**
+ * this method subtract 2 numbers
+ * @param {number} firstNumber
+ * @param {number} secondNumber
+ * @return {number}
+ */
+const subtract = (firstNumber, secondNumber) =>{
+    numberValidation(firstNumber, secondNumber);
+    return firstNumber - secondNumber;
+}
+
+/**
  * this method sum 2 numbers
  * @param {number} firstNumber
  * @param {number} secondNumber
  * @return {number}
  */
 const add = (firstNumber, secondNumber) =>{
+    numberValidation(firstNumber, secondNumber);
     return firstNumber + secondNumber;
 }
 
 module.exports = {
-    add
+    add,
+    subtract
 }
